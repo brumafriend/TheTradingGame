f9cf76120aa1200a8cc1f8e308bc83cc2b4da9c1
tozzzer <33840098+tozzzer@users.noreply.github.com>
2018-01-17 07:55:34 +0000

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,5 +1,4 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtCore import pyqtSlot
